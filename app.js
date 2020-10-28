85bf44b735dc6b868450a467b075ae813f0d9ab8
thaybaldao <thayna.baldao@gmail.com>
2020-10-28 00:28:32 -0300

    upload working

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express') 
-const path = require('path') 
+const path = require('path')
+const multer = require("multer") 
 const app = express() 
   
 // Static Middleware 
@@ -11,6 +12,60 @@ app.set('view engine', 'ejs')
   
 app.get('/', function(req, res){ 
     res.render('Home') 
+})
+    
+var storage = multer.diskStorage({ 
+    destination: function (req, file, cb) { 
+  
+        // uploads is the upload folder name 
+        cb(null, "uploads") 
+    }, 
+    filename: function (req, file, cb) { 
+      cb(null, "dog" + Date.now() + ".jpg") 
+    } 
+  }) 
+       
+// Define the maximum size for uploading
+const maxSize = 5 * 1000 * 1000; // 5 MB
+    
+var upload = multer({  
+    storage: storage, 
+    limits: { fileSize: maxSize }, 
+    fileFilter: function (req, file, cb){ 
+    
+        // Set the filetypes, it is optional 
+        var filetypes = /jpeg|jpg|png/; 
+        var mimetype = filetypes.test(file.mimetype); 
+  
+        var extname = filetypes.test(path.extname(file.originalname).toLowerCase()); 
+        
+        if (mimetype && extname) { 
+            return cb(null, true); 
+        } 
+      
+        cb("Error: File upload only supports the following filetypes - " + filetypes); 
+      }  
+  
+// pic is the name of file attribute 
+}).single("pic");        
+     
+app.post("/upload",function (req, res, next) { 
+        
+    // Error MiddleWare for multer file upload, so if any 
+    // error occurs, the image would not be uploaded! 
+    upload(req,res,function(err) { 
+  
+        if(err) {
+            // ERROR occured (here it can be occured due 
+            // to uploading image of size greater than 
+            // 1MB or uploading different file type) 
+            res.send(err) 
+        } 
+        else { 
+            // SUCCESS, image successfully uploaded 
+            res.send("Success, Image uploaded!") 
+        } 
+    }) 
 }) 
   
 app.listen(8080, function(error){ 
